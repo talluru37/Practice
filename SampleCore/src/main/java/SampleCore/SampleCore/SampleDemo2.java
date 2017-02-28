9c1e3395a2bb5deee56650804f6da8a1d9c1455e
talluru37 <talluru37@gmail.com>
2017-02-28 22:00:19 +0800

    added sampleDemo2 file

---
+++ SampleCore/src/main/java/SampleCore/SampleCore/SampleDemo2.java
@@ -0,0 +1,17 @@
+package SampleCore.SampleCore;
+
+public class SampleDemo2 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("from main2..");
+	}
+	
+	public String testDetails()
+	
+	{
+		System.out.println("from the sample Demo 2 test details");
+		return "sample demo2";
+	}
+
+}
